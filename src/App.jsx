657d704f39446fb8c11bc5426f9f8e40b7b3f24e
Zichler <nikcih99@gmail.com>
2025-02-03 17:45:49 +0100

    Removed react.AIT submodule

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cat from "./components/cat/Cat";
 import Homework01 from "./homeworks/homework01/Homework01";
 import Homework02 from "./homeworks/homework02/Homework02";
 import Lesson01 from "./lessons/lesson01/Lesson01";
+import Lesson03 from "./lessons/lesson03/Lesson03";
 
 function App() {
   return (
