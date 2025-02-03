dd110576e6c3517a037166df18120359d41cbcfa
Zichler <nikcih99@gmail.com>
2025-02-03 19:10:21 +0100

    new deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import Cat from "./components/cat/Cat";
-import Homework01 from "./homeworks/homework01/Homework01";
-import Homework02 from "./homeworks/homework02/Homework02";
-import Lesson01 from "./lessons/lesson01/Lesson01";
-import Lesson03 from "./lessons/lesson03/Lesson03";
+import Homework03 from "./homeworks/homework03/Homework03";
+
 
 function App() {
   return (
     <>
-    <Homework02/>
+    <Homework03/>
     </>
   );
 }
